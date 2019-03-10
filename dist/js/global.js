8275a0e94afcfbdc7ba31e7115b01baa2b56d385
Briana Gude <briana.gude@gmail.com>
2019-03-10 15:00:51 -0400

    added in menu

--- dist/js/global.js
+++ dist/js/global.js
@@ -1,7 +1,13 @@
 const menu = document.querySelector("section.contact");
 const menuButton = document.querySelector("a.menu");
+const closeButton = document.querySelector("a.close-menu");
 
 $(menuButton).click(function() {
-  $(menu).toggleClass("show-menu");
+  $(menu).css("display", "flex");
+  return false;
+});
+
+$(closeButton).click(function() {
+  $(menu).css("display", "none");
   return false;
 });
