c5583c5a786de30f96b4ff9afe7f9aee76db9520
Briana Gude <briana.gude@gmail.com>
2019-03-10 16:07:19 -0400

    added in contentful

--- dist/js/global.js
+++ dist/js/global.js
@@ -11,3 +11,52 @@ $(closeButton).click(function() {
   $(menu).css("display", "none");
   return false;
 });
+
+// contentful
+const spaceId = "ewzub8b1gfrm";
+const environmentId = "master";
+const accessToken =
+  "6cb9ddf756125aa1d52f50a4157a2a05e8363e2c6fc9f95adf3ae1cc915f6a64";
+const url = `https://cdn.contentful.com
+/spaces/${spaceId}/environments/${environmentId}/entries?access_token=${accessToken}`;
+
+const sectionTag = document.querySelector("section.images");
+
+const grabData = function() {
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const assets = data.includes.Asset;
+      return data.items.map(item => {
+        let imageUrl = "image1.jpg";
+        const imageId = item.fields.image.sys.id;
+        const imageData = assets.find(asset => {
+          return asset.sys.id == imageId;
+        });
+        if (imageData) {
+          imageUrl = imageData.fields.file.url;
+        }
+        item.fields.image = imageUrl;
+        return item.fields;
+      });
+    });
+};
+
+grabData().then(data => {
+  console.log(data);
+  sectionTag.innerHTML = "";
+  data.forEach(item => {
+    sectionTag.innerHTML =
+      sectionTag.innerHTML +
+      `
+      <div class="photo">
+        <div class="overlay">
+          <h3>${item.title}</h3>
+          <h5>${item.year}</h5>
+          <p>${item.description}</p>
+        </div>
+        <img src="${item.image}" />
+      </div>
+	`;
+  });
+});
